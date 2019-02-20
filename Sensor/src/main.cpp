ad5a089fc754f908536ce4d32479c54151931109
Amir Ranjbar <iamiranjbar@gmail.com>
2019-02-20 20:26:56 +0330

    sensor code is add.

--- Sensor/src/main.cpp
+++ Sensor/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <Ultrasonic.h>
+
+#define TEMPPIN 1
+
+Ultrasonic ultrasonic(9,8); // (Trig PIN,Echo PIN)
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float temp=analogRead(TEMPPIN); // Reading data from the sensor.This voltage is stored as a 10bit number
+  temp=(5.0*temp*1000.0)/(1024*10);
+  Serial.println(temp);
+  Serial.println(ultrasonic.Ranging(CM));
+
+  /* 5*temp/1024 is to convert the 10 bit number to a voltage reading.
+
+  This is multiplied by 1000 to convert it to millivolt.
+
+  We then divide it by 10 beacuse each degree rise results in a 10 millivolt increase.
+
+  */
+  delay(500);
 }
