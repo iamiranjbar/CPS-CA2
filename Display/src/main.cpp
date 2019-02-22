8cf2ec6e84a03e9535dea0b20ba7b6fbe2c945c8
Amir Ranjbar <iamiranjbar@gmail.com>
2019-02-22 14:44:17 +0330

    ye jaye khoobi..

--- Display/src/main.cpp
+++ Display/src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include <AltSoftSerial.h>
 
 
-#define   CONTRAST_PIN   9
+// #define   CONTRAST_PIN   9
 #define   BACKLIGHT_PIN  7
-#define   CONTRAST       110 
+// #define   CONTRAST       110 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+AltSoftSerial altSerial;
 String s;
+
+typedef union{
+  float fp;
+  byte binary[4];
+} binaryFloat;
 
 void getSensorsData(){
   s ="";
@@ -22,24 +28,37 @@ void getSensorsData(){
   Serial.println(s);
 }
 
+float getUTData(){
+  binaryFloat bn;
+  for(int i = 0; i < 4; i++)
+  { 
+    bn.binary[i] = altSerial.read();
+    Serial.println(bn.binary[i]);
+  }
+  Serial.println(bn.fp);
+  return bn.fp;
+}
+
 void setup()
 {
   Serial.begin ( 9600 );
 
   // Switch on the backlight and LCD contrast levels
-  pinMode(CONTRAST_PIN, OUTPUT);
-  analogWrite ( CONTRAST_PIN, CONTRAST );
+  // pinMode(CONTRAST_PIN, OUTPUT);
+  // analogWrite ( CONTRAST_PIN, CONTRAST );
   lcd.backlight();
 
   lcd.begin(16,4);               // initialize the lcd 
   lcd.home ();                   // go home
-  lcd.setCursor ( 0, 1 );        // go to the next line   
+  altSerial.begin(9600); 
 }
 
 void loop()
 {
   getSensorsData();
+  float UTdata = getUTData();
   lcd.println(s);
+  lcd.println(UTdata);
   delay(250);
   lcd.clear();
 }
--- UT/src/main.cpp
+++ UT/src/main.cpp
@@ -1,9 +1,52 @@
 #include <Arduino.h>
 
+#include <Wire.h>
+
+#define XSTART 10
+#define YSTART 40
+#define BYTENUMBER 4
+
+float get_data_from_registers(int i);
+
 void setup() {
-  // put your setup code here, to run once:
+  Wire.begin();
+  Wire.beginTransmission(110);
+  Wire.write(0);
+  Wire.write(1);
+  Wire.endTransmission();
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+union data
+{
+  byte bytes[4];
+  float result;
+};
+
+
+void loop() {  // put delay for avoid invalid pairing of (x,y) s
+  float x = get_data_from_registers(XSTART);
+  float y = get_data_from_registers(YSTART);
+  // Serial.print("X = ");
+  // Serial.print(x);
+  // Serial.print("      Y = ");
+  // Serial.println(y);
+  float res = pow(x, 2) + pow(y, 2);
+  byte* b = (byte*) &res;
+  Serial.write(b, 4);
+}
+
+
+float get_data_from_registers(int begin_register)
+{
+  union data byte_to_float;
+  for (int i = 0; i < BYTENUMBER; i++)
+  {
+    Wire.beginTransmission(110);
+    Wire.write(begin_register + i);
+    Wire.endTransmission();
+    Wire.requestFrom(110, 1);
+    byte_to_float.bytes[i] = Wire.read();
+  }
+  return byte_to_float.result;
 }
